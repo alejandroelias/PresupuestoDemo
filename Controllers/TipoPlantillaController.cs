e7f32780fb91c6f74ffd23c7dd7070894ad32af9
Alejandro Elias <alejandro.elias@outlook.com>
2022-10-30 16:51:30 -0600

    Modificaciones a tipo plantilla

--- Controllers/TipoPlantillaController.cs
+++ Controllers/TipoPlantillaController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PresupuestoDemo.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +12,32 @@ namespace PresupuestoDemo.Controllers
 {
     public class TipoPlantillaController : Controller
     {
+        private readonly string connectionString;
+        public TipoPlantillaController(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult Crear()
+        public IActionResult Create()
         {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var query = connection.Query("select 1").FirstOrDefault();
+            }
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(TipoPlantilla tipoPlantilla)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View (tipoPlantilla);
+            }
             return View();
         }
     }
---
+++ Models/TipoPlantilla.cs
@@ -0,0 +1,44 @@
+﻿using PresupuestoDemo.Validaciones;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PresupuestoDemo.Models
+{
+    public class TipoPlantilla : IValidatableObject
+    {
+        public int Id_TipoPlantilla { get; set; }
+
+        [Required(ErrorMessage = "El campo Tipo de Plantilla es requerido")]
+        [Display(Name = "Tipo de plantilla")]
+        [PrimeraLetraMayuscula]
+        public string TipoDePlantilla { get; set; }
+
+        [Required(ErrorMessage = "El campo Activa es requerido")]
+        [Display(Name = "Activa")]
+        public bool EsActiva { get; set; }
+
+        public string UsuarioLog { get; set; }
+
+        internal DateTime FechaModificacionLog { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public string Codigo { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Codigo != null && Codigo.Length > 0)
+            {
+                var campoTamano3Letras = Codigo.ToString();
+
+                if (campoTamano3Letras.Length > 3)
+                {
+                    yield return new ValidationResult("El codigo deber solo 3 letras", new[] { nameof(Codigo) });
+                }
+
+            }
+        }
+    }
+}
